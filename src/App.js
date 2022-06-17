3e6ba8fd340ff027e4fdc3adbe446db7e3a15c79
Diwas Dhakal <diwasdhakal75@gmail.com>
2022-06-17 22:42:52 +0545

    -change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@
 import "./App.scss";
 
 function App() {
-   return <div className="name mt-4 fs-1">Diwas Dhakal</div>;
+   return (
+      <>
+         {" "}
+         <div className="name mt-4 fs-1">Diwas Dhakal</div>
+         <div className="description">Hello I am a React Web Developer since 2020</div>
+      </>
+   );
 }
 
 export default App;
